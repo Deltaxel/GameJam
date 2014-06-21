39e6098136594625c3e94ee94a6d82de9290a5d9
payan_a <payan_a@epitech.eu>
2014-06-21 14:09:53 +0200

    Adding sky

--- GameJam/GameJam/main.cpp
+++ GameJam/GameJam/main.cpp
@@ -8,20 +8,32 @@ using namespace sf;
 int main()
 {
 	RenderWindow window(VideoMode(1100, 768, 32), "Mettre ici le nom du jeu", Style::Close | Style::Titlebar);
-	Texture image;
-	Sprite hero;
+	Texture image, sky, clouds, buildings;
+	Sprite hero, sSky, sClouds, sBuildings;
 
-	if (!image.loadFromFile("hero.jpg"))
+	if (image.loadFromFile("hero.jpg") == 0
+		|| sky.loadFromFile("sky.jpg") == 0
+		|| clouds.loadFromFile("clouds.png") == 0
+		|| buildings.loadFromFile("buildings.png") == 0)
 	{
 		std::cerr << "Load image faild" << std::endl;
 		return EXIT_FAILURE;
 	}
-	else
-		hero.setTexture(image);
+	hero.setTexture(image);
 	hero.setPosition(100, 300);
 	hero.scale(0.2,0.2);
+	sSky.setTexture(sky);
+	sSky.setPosition(0, 0);
+	sSky.scale(0, 0);
+	sClouds.setTexture(clouds);
+	sClouds.setPosition(0, 0);
+	sClouds.scale(0, 0);
+	sBuildings.setTexture(buildings);
+	sBuildings.setPosition(0, 0);
+	sBuildings.scale(0, 0);
 	while (window.isOpen())
     {
+		sClouds.setPosition((sClouds.getPosition().x + 1) % 1100, 0);
         Event event;
         while (window.pollEvent(event))
         {
@@ -29,6 +41,9 @@ int main()
                 window.close();
         }
 		window.clear();
+		window.draw(sSky);
+		window.draw(sClouds);
+		window.draw(sBuildings);
 		window.draw(hero);
 		window.display();
     }
